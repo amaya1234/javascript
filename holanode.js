9a9c9b30c77cd74c65f3184bd002578314be1838
Dev1 <Dev1@GET12-07.local>
2018-05-18 13:59:34 +0200

    viernes

--- holanode.js
+++ holanode.js
@@ -1,1 +1,6 @@
 console.log("Este es mi primer node");
+var array = new Array ();
+array.push ("Laura");
+console.log (array[0]);
+
+// hacer una web 
--- imc.js
+++ imc.js
@@ -1,1 +1,6 @@
 //Definir un constructor de persona con los atributos necesarios para poder usarlo en el ejemplo del 3imc.html
+function Persona (peso,altura)
+{
+    this.peso = peso;
+    this.altura = altura;
+}
