982c1f45f4220434a0cbd6ea113c83ddb00ca10b
Dev1 <Dev1@GET12-07.local>
2018-05-23 14:00:16 +0200

    cronometro

--- holanode.js
+++ holanode.js
@@ -4,3 +4,20 @@ array.push ("Laura");
 console.log (array[0]);
 
 // hacer una web 
+/*function palabraDadaVuelta(cadena) {
+
+    var cad_reves = "";
+    var indice;
+    var letraActual;
+
+    for (indice = cadena.length - 1; indice >= 0; indice--) {
+        letraActual = cadena.charAt(indice);
+        cad_reves = cad_reves + letraActual;
+        console.log(" la palabra es....  " + cad_reves);
+    }
+    return cad_reves;
+}
+
+console.log("Este es mi primer node");
+var cadreves = palabraDadaVuelta ("JUANITO");
+console.log ("JUANITO al revés es " + cadreves);*/
--- imc.js
+++ imc.js
@@ -4,3 +4,47 @@ function Persona (peso,altura)
     this.peso = peso;
     this.altura = altura;
 }
+Persona.prototype=
+{
+    calcularIMC_n : function ()
+    {
+        var imc = 0;
+            imc = this.peso / (this.altura * this.altura);
+        return imc;
+    },
+    calcularIMC_l : function ()
+    {
+        var imc_l ="";
+        var imc_n = this.calcularIMC_n();
+
+            if (imc_n < 16) {
+                var imc_l = 'DESNUTRIDO';
+            } else {
+                if (imc_n >= 16 && imc_n < 18) {
+                    var imc_l = 'DELGADO';
+                } else {
+                    if (imc_n >= 18 && imc_n < 25) {
+                        var imc_l = 'IDEAL';
+                    } else {
+                        if (imc_n >= 25 && imc_n < 31) {
+                            var imc_l = 'SOBREPESO';
+                        } else {
+                            var imc_l = 'OBESO';
+                        }
+                    }
+                }
+            }
+        return imc_l; 
+    }
+}
+
+/*var persona = new Persona (69,1.67);
+
+console.log (persona.peso);
+console.log (persona.altura);
+console.log ("proto persona");
+console.log (persona.__proto__);
+var imc_n = persona.calcularIMC_n();
+var imc_l = persona.calcularIMC_l();
+console.log("El imc numérico es : "+imc_n+" y el imc literal es : "+ imc_l);
+*/
